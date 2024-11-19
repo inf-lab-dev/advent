f7e2d4558a9bd2a2ad2a989afdae63d1fe43dfab
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-19 16:59:01 +0100

    Move `app` into `src`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss';
 export default {
     content: [
         './src/components/**/*.{js,ts,tsx}',
-        './app/**/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {
