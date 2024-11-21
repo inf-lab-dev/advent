e1af86aa962c1668b675410112ce190d41e91585
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-21 18:32:18 +0100

    Add separator component

--- src/components/layout/error.tsx
+++ src/components/layout/error.tsx
@@ -1,5 +1,6 @@
 import { CandyCane, Snowflake, TreePine } from 'lucide-react';
 import { PropsWithChildren } from 'react';
+import { Separator } from '../ui/separator';
 
 export type Icon = 'tree' | 'cane' | 'snowflake';
 
@@ -28,8 +29,10 @@ export default function ErrorPage({ icon, code, text, children }: Props) {
     return (
         <div className="flex min-h-[70vh] flex-col items-center justify-center gap-5">
             <div className="flex items-center gap-3 text-[5rem]">
-                <IconComponent
-                    className={`${iconClasses} h-[5rem] w-[5rem] border-r-2 border-muted pr-3 dark:border-muted-foreground`}
+                <IconComponent className={`${iconClasses} h-[5rem] w-[5rem]`} />
+                <Separator
+                    className="h-[5rem] w-[2px]"
+                    orientation="vertical"
                 />
                 <h1 className="font-bold">{code}</h1>
             </div>
