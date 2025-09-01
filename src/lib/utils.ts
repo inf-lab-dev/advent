673858054de8d1272a803b4004ac6fbe1a07fba4
Kuschel-Swein <kuschelswein@gmail.com>
2025-09-01 17:58:47 +0200

    Upgrade shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-/**
- * Merges and resolves tailwind classes.
- *
- * @param inputs the inputs supportd by `clsx`
- * @returns the merged classes
- */
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
